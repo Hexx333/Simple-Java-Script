8ec9f558d7f2b2921b00fcd61e09ac455d38c2d5
Hexx333 <116751160+Hexx333@users.noreply.github.com>
2024-01-03 21:46:51 +0100

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,5 @@
+var total = 131; //number
+total = "This is JAvaScript"; //string
+total = {"Customer":"Karen Mc Grath"}; //object
+total f= function({alert("Hello")}; //function
+total(); => Hello ( executes function)
